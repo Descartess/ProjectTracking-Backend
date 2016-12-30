5c3f3fe1ff4f7f5ab2e507056552c9180aac6653
NyondoPaul <nyondopaul23@gmail.com>
2016-12-30 15:49:59 +0300

    Fixed crash

--- proj_track/asgi.py
+++ proj_track/asgi.py
@@ -2,6 +2,6 @@
 import os
 import channels.asgi
 
-oe.environ.setdefault("DJANGO_SETTINGS_MODULE","proj_track.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","proj_track.settings")
 
 channel_layer = channels.asgi.get_channel_layer()
